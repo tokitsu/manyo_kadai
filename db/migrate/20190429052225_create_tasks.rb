15c614bd2d7e4930f954c658c2c9b9dfc26c69c3
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-04-29 17:12:11 +0900

    add crud function

--- db/migrate/20190429052225_create_tasks.rb
+++ db/migrate/20190429052225_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-
+      t.string :name
+      t.text :content
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2019_04_29_052225) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
