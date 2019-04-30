2caaeacae1efad30d0c8d537d20c862e07f511fd
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-04-30 14:14:09 +0900

    add feature spec in task spec

---
+++ spec/features/task.spec.rb
@@ -0,0 +1,42 @@
+# このrequireで、Capybaraなどの、Feature Specに必要な機能を使用可能な状態にしています
+require 'rails_helper'
+
+# このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
+RSpec.feature "タスク管理機能", type: :feature do
+  # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
+  scenario "タスク一覧のテスト" do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    Task.create!(name: 'test_task_01', content: 'testtesttest')
+    Task.create!(name: 'test_task_02', content: 'samplesample')
+
+  # tasks_pathにvisitする（タスク一覧ページに遷移する）
+    visit tasks_path
+
+  # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
+  # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
+    expect(page).to have_content 'testtesttest'
+    expect(page).to have_content 'samplesample'
+  end
+
+  scenario "タスク作成のテスト" do
+
+    visit new_task_path
+
+    fill_in "名前", with: "com"
+    fill_in "内容", with: "www"
+
+
+    click_on '投稿する'
+
+    expect(page).to have_content '新規登録しました!'
+  end
+
+  scenario "タスク詳細のテスト" do
+
+    task = Task.create!(name: 'test55',content: 'test333')
+
+    visit task_path(task.id)
+
+    expect(page).to have_content 'test333'
+  end
+end
