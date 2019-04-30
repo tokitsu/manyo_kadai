7730415efab46fba36ab79f27adf85137be10ab6
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-04-30 15:56:58 +0900

    add timezone in application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module Sample
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
