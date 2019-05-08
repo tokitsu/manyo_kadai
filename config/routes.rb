770b221b40c7d1a3bc3a85a73aac990fc429a932
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-05-08 20:12:28 +0900

    add login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: "tasks#index"
   resources:tasks
+  resources:users
+  resources:sessions, only:[:new,:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
